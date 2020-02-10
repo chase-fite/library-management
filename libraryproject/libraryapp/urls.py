f929168edc257a956af02b9e4474fe6a29ddcc55
Chase Fite <chasef7031@gmail.com>
2020-02-10 08:52:14 -0600

    finished chapter 4

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('books/', book_list, name='books'),
     path('librarians/', librarian_list, name='librarians'),
     path('libraries/', library_list, name='libraries'),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout')
 ]
