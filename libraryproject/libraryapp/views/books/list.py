44cc2e4aaee3ef614f7d98bca88355c1a8d07aec
Chase Fite <chasef7031@gmail.com>
2020-02-10 12:36:23 -0600

    finished chapter 5

--- libraryproject/libraryapp/views/books/list.py
+++ libraryproject/libraryapp/views/books/list.py
@@ -1,9 +1,9 @@
 import sqlite3
-from django.shortcuts import render
-from libraryapp.models import Book
+from django.shortcuts import render, redirect, reverse
+from libraryapp.models import Book, model_factory
 from ..connection import Connection
-from libraryapp.models import model_factory
 from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def book_list(request):
@@ -32,3 +32,23 @@ def book_list(request):
         }
 
         return render(request, template, context)
+    
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO libraryapp_book
+            (
+                title, author, ISBN_number,
+                year_published, location_id, librarian_id
+            )
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (form_data['title'], form_data['author'],
+                str(form_data['ISBN_number']), form_data['year_published'],
+                request.user.librarian.id, form_data["location"]))
+
+        return redirect(reverse('libraryapp:books'))
